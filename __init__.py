7b6e08584f97d27b84b9d2fb97cfc31652ebc1a1
s1lmn <lev040108@gmail.com>
2026-05-10 14:51:01 +0300

    task - 2

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .cli import ConsoleInterface
-
-__all__ = ["ConsoleInterface"]
---
+++ src/db/backend/__init__.py
@@ -0,0 +1,3 @@
+from .memory import Student, StudentRecord, create_record, select_record
+
+__all__ = ["Student", "StudentRecord", "create_record", "select_record"]
